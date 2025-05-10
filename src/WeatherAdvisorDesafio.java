033d1b06aad215fbf63d438a0976cce50756e16f
josuadbl <josuabolivarlondono@gmail.com>
2025-05-09 21:23:43 -0500

    Desafio FizzBuzz mejora

--- src/WeatherAdvisorDesafio.java
+++ src/WeatherAdvisorDesafio.java
@@ -1,19 +1,41 @@
+import java.util.Scanner;
+
+
 public class WeatherAdvisorDesafio {
      public static void main(String[] args) {
         
-        for (int i = 1; i <= 100; i++) {
+         Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Ingresa el número inicial: ");
+        int inicio = scanner.nextInt();
+
+        System.out.print("Ingresa el número final: ");
+        int fin = scanner.nextInt();
+
+        
+        for (int i = inicio; i <= fin; i++) {
+            String resultado = "";
+
             
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
+            if (i % 3 == 0) {
+                resultado += "Fizz";
+            }
+            if (i % 5 == 0) {
+                resultado += "Buzz";
+            }
+            if (i % 7 == 0) {
+                resultado += "Boom";
+            }
+
+            
+            if (resultado.isEmpty()) {
                 System.out.println(i);
+            } else {
+                System.out.println(resultado);
             }
         }
+
+        scanner.close();
     }
 }
-
-
